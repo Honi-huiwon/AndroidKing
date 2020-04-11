6a86ad339bb6fa1bb2f3350812c24b2ac913c05d
gml3387 <gml3387@naver.com>
2020-04-11 15:54:39 +0900

    hi i m heewon

---
+++ app/src/main/java/com/example/androidking/LoginActivity.kt
@@ -0,0 +1,43 @@
+package com.example.androidking
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.backgroundResource
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        editLoginID.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) v.setBackgroundResource(R.drawable.primaryellow_border)
+            else v.setBackgroundResource(R.drawable.gray_border)
+        }
+        editLoginPW.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) v.setBackgroundResource(R.drawable.primaryellow_border)
+            else v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        btnLoginSubmit.setOnClickListener{
+            val login_u_id = editLoginID.text.toString()
+            val login_u_pw = editLoginPW.text.toString()
+
+            if(login_u_id == "") editLoginID.requestFocus()
+            else if(login_u_pw =="") editLoginPW.requestFocus()
+            /*else postLoginResponse(login_u_id,login_u_pw)*/
+        }
+
+        txtLoginSignup.setOnClickListener {
+            val intent: Intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        /*fun postLoginResponse(u_id:String, u_pw:String){
+            finish()
+        }*/
+        val id = intent.getIntExtra("id",127)
+    }
+}
